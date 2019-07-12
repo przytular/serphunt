101a269e6d6323c2188c680d840651bc0e468c67
Radosław Przytuła <radoslaw.przytula@gmail.com>
2019-07-12 17:15:01 +0200

    change model name, make user field optional

--- serp/models.py
+++ serp/models.py
@@ -7,8 +7,8 @@ class UserConfig(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
-class Search(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
+class SearchResults(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 	created = models.DateTimeField(auto_now_add=True)
 	ip = models.GenericIPAddressField()
 	keywords = models.CharField(max_length=255)
