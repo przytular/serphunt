56edc820d2d521c5886a40b0ade2adebe688e9e0
Radosław Przytuła <radoslaw.przytula@gmail.com>
2019-07-12 16:37:27 +0200

    add prepare tables for results

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,34 @@
-import 'bootstrap';
 import '../scss/app.scss';
+
+import $ from 'jquery';
+import 'bootstrap';
+import 'datatables.net';
+import 'datatables.net-dt/css/jquery.dataTables.css';
+import 'datatables.net-bs4';
+
+
+let data = [
+	[1, "Example.com"]
+]
+
+$(document).ready( function () {
+
+	let $form = $("form#search");
+	let config = {
+		"lengthChange": false,
+		"info": false,
+		"paging": false,
+		"language": {
+			"search": ""
+		}
+	}
+
+	$form.submit(function (e) {
+		e.preventDefault();
+		let data = $form.serializeArray();
+		console.log(data);
+	});
+
+	$('table#top10').DataTable(config);
+	$('table#keywords').DataTable(config);
+} );
