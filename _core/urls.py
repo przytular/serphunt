6e83dae495413b4ac47645dce08a547f61813c11
Radosław Przytuła <radoslaw.przytula@gmail.com>
2019-07-12 15:12:52 +0200

    add basic templates

--- _core/urls.py
+++ _core/urls.py
@@ -20,6 +20,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
 	path('', TemplateView.as_view(template_name='index.html'), name='home'),
+	path('history/', TemplateView.as_view(template_name='history.html'), name='history'),
 
     path('admin/', admin.site.urls),
 ]
