f86391f25ebfecc8cf1d01f5d48966271b9f4cef
Radosław Przytuła <radoslaw.przytula@gmail.com>
2019-07-12 15:06:31 +0200

    add models and make migrations

--- serp/models.py
+++ serp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+
+class UserConfig(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Search(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	created = models.DateTimeField(auto_now_add=True)
+	ip = models.GenericIPAddressField()
+	keywords = models.CharField(max_length=255)
+	results = JSONField()
