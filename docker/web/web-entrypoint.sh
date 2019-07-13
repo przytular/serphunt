cfe1075cd02f02142161986d51ffc4c7a6908951
Radosław Przytuła <radoslaw.przytula@gmail.com>
2019-07-13 10:31:45 +0200

    add nginx docker container

--- docker/web/web-entrypoint.sh
+++ docker/web/web-entrypoint.sh
@@ -14,3 +14,6 @@ npm start &
 
 # Start django development server
 python manage.py runserver 0.0.0.0:8000
+
+# Start production server (gunicorn)
+# gunicorn _core.wsgi:application -b 0.0.0.0:8000 --reload
