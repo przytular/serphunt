52d33d09df43b542edd68dd5dd2590f23529dca3
Radosław Przytuła <radoslaw.przytula@gmail.com>
2019-07-15 08:07:51 +0200

    add serp time limit

--- serp/views.py
+++ serp/views.py
@@ -1,8 +1,10 @@
+import datetime
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.conf import settings
 
 from rest_framework import viewsets, mixins
 from rest_framework.response import Response
@@ -35,11 +37,24 @@ class SearchViewSet(viewsets.GenericViewSet,
 	def create(self, request, *args, **kwargs):
 		data = request.data.dict()
 		keyword = data.get('keyword')
+		data['user'] = request.user.pk if request.user.is_authenticated else None
+
+		# Check if search results already exists and check time limit.
+		sr = SearchResults.objects.filter(
+			keyword=keyword,
+			user=data['user'])
+		sr = sr.first()
+		if sr:
+			time_limit = sr.created + datetime.timedelta(seconds=settings.SERP_TIME_LIMIT)
+			if datetime.datetime.now() < time_limit:
+				serializer = self.serializer_class(sr)
+				return Response(serializer.data)
+
+		# Update data dictionary with mandatory fields
 		data['ip'] = request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('REMOTE_ADDR', '127.0.0.1'))
 		data.update({"results": get_google_results(keyword)} if keyword else {"results": ""})
-		if request.user.is_authenticated:
-			data['user'] = request.user
 
+		# Serializer validation and return.
 		serializer = self.serializer_class(data=data)
 		if serializer.is_valid():
 			serializer.save()
